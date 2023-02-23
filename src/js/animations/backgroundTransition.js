6e86a9bc72fa15548b2817eb5ed93f4c7eb60110
deviloche <abeer.khan@olivedigital.com>
2023-02-23 05:07:55 +0500

    Lenis disabled for touch devices

--- src/js/animations/backgroundTransition.js
+++ src/js/animations/backgroundTransition.js
@@ -12,8 +12,8 @@ export default function backgroundTransition() {
         trigger: section,
         start: 'top center',
         end: () => `+=${section.offsetHeight}px`,
-        invalidateOnRefresh: true,
-        markers: true,
+        // invalidateOnRefresh: true,
+        // markers: true,
         onToggle() {
           gsap.to('body', {
             backgroundColor: colorAttr === 'dark' ? gsap.getProperty('html', '--dark-color') : gsap.getProperty('html', '--light-color'),
